709ecc625fd4688a19a3deddae491e2977cf4778
Arif <ayon.arif.10@gmail.com>
2026-01-30 12:31:02 +0600

    forgot-password & reset-password implemented

--- app/Livewire/Auth/ForgotPasswordPage.php
+++ app/Livewire/Auth/ForgotPasswordPage.php
@@ -2,10 +2,31 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Password;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
+#[Title('Forgot Password Page')]
 class ForgotPasswordPage extends Component
 {
+    #[Validate('required|email|exists:App\Models\User,email')]
+    public $email;
+
+    public function save() {
+       $this->validate();
+       
+        $status = Password::sendResetLink(['email' => $this->email]);
+
+        if($status === Password::RESET_LINK_SENT) {
+            session()->flash('success', 'Password reset link has been sent to your email address');
+            $this->email = '';
+        } else {
+            session()->flash('error', 'Something went wrong');
+        }
+    }
+
+
     public function render()
     {
         return view('livewire.auth.forgot-password-page');
